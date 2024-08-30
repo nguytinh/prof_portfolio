54e7654e8ee368ddbc99723b487ef68890176cd6
Tinhphong Nguyen <44383317+nguytinh@users.noreply.github.com>
2024-08-30 16:46:05 -0700

    navbar bug fixes

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -7,29 +7,62 @@ function NavigationBar() {
   const [expanded, setExpanded] = useState(false);
   const location = useLocation();
 
-  const getActiveLinkClass = (path) => 
+  const getActiveLinkClass = (path) =>
     `nav-link ${location.pathname === path ? 'active' : ''}`;
 
   return (
     <>
-      <Navbar expanded={expanded} expand="lg" bg="white" className="border-bottom-0 shadow-none" style={{ boxShadow: 'none', paddingTop: '20px'}}>
+      <Navbar
+        expanded={expanded}
+        expand="lg"
+        bg="white"
+        className="border-bottom-0 shadow-none"
+        style={{ boxShadow: 'none', paddingTop: '20px' }}
+      >
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
+          {/* Toggle Button for Collapsible Navbar, aligned to the right */}
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={() => setExpanded(expanded ? false : 'expanded')}
+            style={{ borderColor: 'transparent', marginLeft: 'auto' }} // Aligns the toggle button to the right
+          >
+            {/* Custom icon for the toggle button */}
+            <span style={{ color: 'black' }}>&#9776;</span>
+          </Navbar.Toggle>
+
+          {/* Collapsible Navbar Links */}
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link as={NavLink} to="/" className={getActiveLinkClass('/')} onClick={() => setExpanded(false)}>
+            <Nav className="mx-auto text-center"> {/* Center the Nav items */}
+              <Nav.Link
+                as={NavLink}
+                to="/"
+                className={getActiveLinkClass('/')}
+                onClick={() => setExpanded(false)}
+              >
                 Home
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/about" className={getActiveLinkClass('/about')} onClick={() => setExpanded(false)}>
+              <Nav.Link
+                as={NavLink}
+                to="/about"
+                className={getActiveLinkClass('/about')}
+                onClick={() => setExpanded(false)}
+              >
                 About
               </Nav.Link>
-              <Nav.Link as={NavLink} to="/contact" className={getActiveLinkClass('/contact')} onClick={() => setExpanded(false)}>
+              <Nav.Link
+                as={NavLink}
+                to="/contact"
+                className={getActiveLinkClass('/contact')}
+                onClick={() => setExpanded(false)}
+              >
                 Contact
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* Divider for styling below the navbar */}
       <div className="wrapper">
         <div className="divider div-transparent div-arrow-down"></div>
       </div>
