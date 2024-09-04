a8330f0288ed3234fbeaae3d05f896931ed3f56e
Tinhphong Nguyen <44383317+nguytinh@users.noreply.github.com>
2024-09-03 18:58:09 -0700

    added styling in about page

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { scroller } from 'react-scroll';
 import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardText, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
-import "../styles/About.css";
+import styles from "../styles/About.module.css";
 import resume from '../assets/Tinh_s_Resume.pdf';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
@@ -32,81 +32,80 @@ function AboutMe() {
       style={{ position: 'relative', overflow: 'hidden' }} // Ensure the animation fits the container
     >
       <svg
-  version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlnsXlink="http://www.w3.org/1999/xlink"
-  x="0px"
-  y="0px"
-  width="100%"
-  height="100%"
-  viewBox="0 0 1600 900"
-  preserveAspectRatio="xMidYMax slice"
-  style={{
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    zIndex: -1, // Place behind other content
-    backgroundColor: '#ffffff', // White background
-  }}
->
-  <defs>
-    <linearGradient id="bg">
-      <stop offset="0%" style={{ stopColor: 'rgba(240, 240, 240, 0.06)' }} />
-      <stop offset="50%" style={{ stopColor: 'rgba(200, 200, 200, 0.6)' }} />
-      <stop offset="100%" style={{ stopColor: 'rgba(180, 180, 180, 0.2)' }} />
-    </linearGradient>
-    <path
-      id="wave"
-      fill="url(#bg)"
-      d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
-        s371.981,38.998,575.971,0s293.985-39.278,505.474,0s493.475,48.368,716.963-25.995v560.106H-363.852V502.589z"
-    />
-  </defs>
-  <g>
-    <use xlinkHref="#wave" opacity=".3">
-      <animateTransform
-        attributeName="transform"
-        attributeType="XML"
-        type="translate"
-        dur="10s"
-        calcMode="spline"
-        values="270 230; -334 180; 270 230"
-        keyTimes="0; .5; 1"
-        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-        repeatCount="indefinite"
-      />
-    </use>
-    <use xlinkHref="#wave" opacity=".6">
-      <animateTransform
-        attributeName="transform"
-        attributeType="XML"
-        type="translate"
-        dur="8s"
-        calcMode="spline"
-        values="-270 230;243 220;-270 230"
-        keyTimes="0; .6; 1"
-        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-        repeatCount="indefinite"
-      />
-    </use>
-    <use xlinkHref="#wave" opacity=".9">
-      <animateTransform
-        attributeName="transform"
-        attributeType="XML"
-        type="translate"
-        dur="6s"
-        calcMode="spline"
-        values="0 230;-140 200;0 230"
-        keyTimes="0; .4; 1"
-        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-        repeatCount="indefinite"
-      />
-    </use>
-  </g>
-</svg>
-
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1600 900"
+        preserveAspectRatio="xMidYMax slice"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1, // Place behind other content
+          backgroundColor: '#ffffff', // White background
+        }}
+      >
+        <defs>
+          <linearGradient id="bg">
+            <stop offset="0%" style={{ stopColor: 'rgba(240, 240, 240, 0.06)' }} />
+            <stop offset="50%" style={{ stopColor: 'rgba(200, 200, 200, 0.6)' }} />
+            <stop offset="100%" style={{ stopColor: 'rgba(180, 180, 180, 0.2)' }} />
+          </linearGradient>
+          <path
+            id="wave"
+            fill="url(#bg)"
+            d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+              s371.981,38.998,575.971,0s293.985-39.278,505.474,0s493.475,48.368,716.963-25.995v560.106H-363.852V502.589z"
+          />
+        </defs>
+        <g>
+          <use xlinkHref="#wave" opacity=".3">
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="translate"
+              dur="10s"
+              calcMode="spline"
+              values="270 230; -334 180; 270 230"
+              keyTimes="0; .5; 1"
+              keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+              repeatCount="indefinite"
+            />
+          </use>
+          <use xlinkHref="#wave" opacity=".6">
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="translate"
+              dur="8s"
+              calcMode="spline"
+              values="-270 230;243 220;-270 230"
+              keyTimes="0; .6; 1"
+              keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+              repeatCount="indefinite"
+            />
+          </use>
+          <use xlinkHref="#wave" opacity=".9">
+            <animateTransform
+              attributeName="transform"
+              attributeType="XML"
+              type="translate"
+              dur="6s"
+              calcMode="spline"
+              values="0 230;-140 200;0 230"
+              keyTimes="0; .4; 1"
+              keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+              repeatCount="indefinite"
+            />
+          </use>
+        </g>
+      </svg>
 
       <MDBContainer className="my-5">
         <MDBRow id="top" className="text-center mb-4">
@@ -139,12 +138,13 @@ function AboutMe() {
         <MDBRow className="text-center mb-4">
           <MDBCol md="4" className="d-flex justify-content-center mx-auto">
             <MDBCard style={{ width: '100%', border: 'none', boxShadow: 'none'}}>
-              <img
-                src={profileImage}
-                alt="Profile"
-                className="img-fluid"
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-              />
+              <div className={styles.imgWrapper}>
+                <img
+                  src={profileImage}
+                  alt="Profile"
+                  className={styles.imgFluid}
+                />
+              </div>
             </MDBCard>
           </MDBCol>
         </MDBRow>
