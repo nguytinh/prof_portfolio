92786351775621724a6269a29aac080f47ba6d05
Tinhphong Nguyen <44383317+nguytinh@users.noreply.github.com>
2024-09-05 04:22:21 -0700

    finished more styling

--- portfolio/src/components/MayLike.jsx
+++ portfolio/src/components/MayLike.jsx
@@ -59,29 +59,29 @@ function MayLike({ currentProject }) {
                 }}
                 className="card-hover"
               >
-                <div className="bg-image" style={{ borderRadius: '20px' }}>
+                <div className="bg-image" style={{ borderRadius: '0px' }}>
                   <img
                     src={project.image}
                     className="w-100"
                     alt={project.title}
-                    style={{ height: '300px', objectFit: 'cover', borderRadius: '20px' }}
+                    style={{ height: '300px', objectFit: 'cover', borderRadius: '0px' }}
                   />
                   <div
                     className="mask d-flex align-items-center justify-content-center"
                     style={{
-                      background: 'linear-gradient(45deg, rgba(0, 0, 0, 0.8), rgba(150, 150, 150, 0.4))',
-                      borderRadius: '20px',
+                      background: 'linear-gradient(45deg, rgba(100, 100, 100, 0.6), rgba(250, 250, 250, 1))',
+                      borderRadius: '0px',
                       position: 'absolute',
                       top: 0,
                       left: 0,
                       right: 0,
                       bottom: 0,
-                      color: 'white',
+                      color: 'black',
                     }}
                   >
                     <MDBCardBody style={{ textAlign: 'center' }}>
                       <MDBCardTitle>{project.title}</MDBCardTitle>
-                      <MDBCardText style={{ fontSize: '16px' }}>{project.year}</MDBCardText>
+                      <MDBCardText style={{ fontSize: '16px', textDecorationThickness: "10px" }}>{project.year}</MDBCardText>
                     </MDBCardBody>
                   </div>
                 </div>
@@ -95,7 +95,7 @@ function MayLike({ currentProject }) {
         .card-hover:hover {
           transform: scale(1.05);
           box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-          outline: 3px solid rgba(0, 0, 0, 0.6); /* Use outline instead of border */
+          outline: 3px solid rgba(250, 250, 250, 0.8); /* Use outline instead of border */
         }
       `}</style>
     </div>
--- portfolio/src/pages/More.jsx
+++ portfolio/src/pages/More.jsx
@@ -55,7 +55,6 @@ function More() {
             style={{
               fontSize: '3rem',
               color: 'black', 
-              textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)' 
             }}
           >
             My Photos
